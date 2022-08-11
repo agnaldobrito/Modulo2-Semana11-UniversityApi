217e6b90b3265f34ea0cec179000ef6df7fee921
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-08-11 14:53:41 -0300

    refactor: adicionando DbSet da entidade Course

--- UniversityApi/Context/UniversityContext.cs
+++ UniversityApi/Context/UniversityContext.cs
@@ -11,5 +11,6 @@ namespace UniversityApi.Context
 
         public DbSet<Student> Students { get; set; }
         public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Course> Courses { get; set; }
     }
 }
