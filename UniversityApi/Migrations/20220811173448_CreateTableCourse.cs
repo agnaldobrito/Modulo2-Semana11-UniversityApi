0d58fec6f49f65ce6094552d6555c253a50f526e
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-08-11 14:55:03 -0300

    refactor: criação da tabela course no bd

---
+++ UniversityApi/Migrations/20220811173448_CreateTableCourse.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UniversityApi.Migrations
+{
+    public partial class CreateTableCourse : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Curso",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    Requisito = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    CargaHoraria = table.Column<int>(type: "int", nullable: true),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Curso", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Curso");
+        }
+    }
+}
