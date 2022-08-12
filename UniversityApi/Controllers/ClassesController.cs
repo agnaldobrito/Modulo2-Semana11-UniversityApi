e4fd63a8981c4b22bbfa416b5bad90d9025798e8
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-08-12 09:04:01 -0300

    add: controllers das entidades class e registration

---
+++ UniversityApi/Controllers/ClassesController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniversityApi.Models;
+using UniversityApi.Context;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace University2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClassesController : ControllerBase
+    {
+        private UniversityContext _universityContext;
+        public ClassesController(UniversityContext universityContext)
+        {
+            _universityContext = universityContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Class>>> Get()
+        {
+            try
+            {
+                IEnumerable<Class> classes = await _universityContext.Classes
+                    .Include(x => x.Course)
+                    .Include(y => y.Course)
+                    .ToListAsync();
+
+                return classes.Any() ? Ok(classes) : StatusCode(404);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Class>> GetById(int id)
+        {
+            try
+            {
+                Class class1 = await _universityContext.Classes
+                    .Include(x => x.Course)
+                    .Include(y => y.Instructor)
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                return class1 is not null ? Ok(class1) : StatusCode(404);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Class class1)
+        {
+            try
+            {
+                _universityContext.Classes.Add(class1);
+                await _universityContext.SaveChangesAsync();
+
+                return CreatedAtAction("GetById", new { Id = class1.Id }, class1);
+
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromBody] Class class1)
+        {
+            try
+            {
+                bool classExist = _universityContext.Classes.Any(x => x.Id == class1.Id);
+                if (!classExist) return NotFound();
+
+                _universityContext.Entry(class1).State = EntityState.Modified;
+                await _universityContext.SaveChangesAsync();
+
+                return StatusCode(202);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var class1 = await _universityContext.Classes.FindAsync(id);
+
+                if (class1 is null) return NotFound();
+
+                _universityContext.Classes.Remove(class1);
+                await _universityContext.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ UniversityApi/Controllers/RegistrationsController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniversityApi.Context;
+using UniversityApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UniversityApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationsController : ControllerBase
+    {
+        private UniversityContext _universityContext;
+        public RegistrationsController(UniversityContext universityContext)
+        {
+            _universityContext = universityContext;
+        }
+        // GET: api/<RegistrationController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var registrations = await _universityContext.Registrations
+                    .Include(x => x.Class)
+                    .Include(z => z.Class.Instructor)
+                    .Include(w => w.Class.Course)
+                    .Include(y => y.Student)
+                    .ToListAsync();
+
+                return registrations.Any() ? Ok(registrations) : NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        // GET api/<RegistrationController>/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var registrations = await _universityContext.Registrations
+                    .Include(x => x.Class)
+                    .Include(z => z.Class.Instructor)
+                    .Include(w => w.Class.Course)
+                    .Include(y => y.Student)
+                    .FirstOrDefaultAsync();
+
+                return registrations is not null ? Ok(registrations) : NotFound();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        // POST api/<RegistrationController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Registration registration)
+        {
+            try
+            {
+                _universityContext.Registrations.Add(registration);
+                await _universityContext.SaveChangesAsync();
+
+                return CreatedAtAction("GetById", new { Id = registration.Id }, registration);
+
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        // PUT api/<RegistrationController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromBody] Registration registration)
+        {
+            try
+            {
+                bool registrationExist = _universityContext.Registrations.Any(x => x.Id == registration.Id);
+                if (!registrationExist) return NotFound();
+
+                _universityContext.Entry(registration).State = EntityState.Modified;
+                await _universityContext.SaveChangesAsync();
+
+                return StatusCode(202);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        // DELETE api/<RegistrationController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                var registration = await _universityContext.Registrations.FindAsync(id);
+
+                if (registration is null) return NotFound();
+
+                _universityContext.Registrations.Remove(registration);
+                await _universityContext.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
